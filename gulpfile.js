039cbf08dfc8e7cc4f594f470ebfec5b4324726a
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-05-08 16:14:50 +0400

    build config added

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+
+var browserify  = require('browserify');
+var babelify    = require('babelify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var uglify      = require('gulp-uglify');
+var sourcemaps  = require('gulp-sourcemaps');
+
+
+gulp.task('build', function () {
+    // app.js is your main JS file with all your module inclusions
+    return browserify({entries: './src/control/Validation.js', debug: true})
+        .transform(babelify, { presets: ["es2015"], global: true })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify().on('error', function(err) { console.log(err.toString()) }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('./src/*.js', ['build']);
+});
+
+gulp.task('default', ['watch']);
